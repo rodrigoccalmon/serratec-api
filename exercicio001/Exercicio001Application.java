621711464973242dcf9874b204ceb6193c1f4481
Rodrigo Calmon <89034960+rodrigoccalmon@users.noreply.github.com>
2021-10-27 11:46:40 -0300

    Add files via upload

---
+++ exercicio001/Exercicio001Application.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.exercicio001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio001Application.class, args);
+	}
+
+}
