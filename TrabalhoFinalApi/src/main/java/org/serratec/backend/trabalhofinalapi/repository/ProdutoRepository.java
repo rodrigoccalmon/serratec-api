cfc8a8501cf3f9ff87c665f34abd1a6a31c4588d
Rodrigo Calmon <89034960+rodrigoccalmon@users.noreply.github.com>
2021-11-12 17:49:58 -0300

    Delete TrabalhoFinalApi directory

--- TrabalhoFinalApi/src/main/java/org/serratec/backend/trabalhofinalapi/repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package org.serratec.backend.trabalhofinalapi.repository;
-
-import org.serratec.backend.trabalhofinalapi.domain.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-
-}
